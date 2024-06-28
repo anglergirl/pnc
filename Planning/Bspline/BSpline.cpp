4cbea837b8e69b111a6e548f7951e876510a6817
anglergirl <63649261+anglergirl@users.noreply.github.com>
2024-06-28 11:48:03 +0800

    Update BSpline.cpp

--- Planning/Bspline/BSpline.cpp
+++ Planning/Bspline/BSpline.cpp
@@ -30,3 +30,20 @@ double baseFunction(int i, int k, double u, vector<double> node_vector) {
 
   return Bik_u;
 }
+
+/*
+ 准均匀B样条的节点向量计算
+ 首末值定义为0 和 1
+ @param n 控制点个数 - 1，控制点共 n + 1个
+ @param k B样条阶数k， k阶B样条， k-1次曲线
+ @return
+*/
+vector<double> u_quasi_uniform(int n, int k) {
+  vector<double> node_vector(n + k + 1);    //准均匀B样条的节点向量计算，共n+1个控制点，k-1次B样条，k阶
+  double picewise = n - k + 2;   //B样条曲线的段数：控制点个数 - 次数
+  if(piecewise == 1) {  //只有一段曲线时， n = k + 1
+    for(int i = n + 1; i < n + k + 1; i++) node_vector[i] = 1
+    
+  }
+ 
+}
