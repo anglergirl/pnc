4fda20ff1549974eaa3e3618abe4208b17b2e69f
anglergirl <63649261+anglergirl@users.noreply.github.com>
2024-07-03 11:16:34 +0800

    Update LQRControl.cpp

--- Tracking/LQR/LQRControl.cpp
+++ Tracking/LQR/LQRControl.cpp
@@ -1,1 +1,43 @@
-#include<iostream>
+#include "LQRControl.h"
+
+LQRControl::LQRControl(int n) : N(n) {}
+
+/*
+  解代数里卡提方程
+  @param A 状态矩阵A
+  @param B 状态矩阵B
+  @param Q Q为半正定的状态加权矩阵，通常取为对角阵；Q矩阵元素变大意味着希望跟踪偏差能够快速趋紧于零
+  @param R R为正定的控制加权矩阵，R矩阵元素变大意味着希望控制输入能够尽可能小
+  @return
+*/
+MatrixXd LQRControl::calRicatti(MatrixXd A, MatrixXd B, MatrixXd Q, MatrixXd R) {
+  MatrixXd Qf = Q;
+  MatrixXd P = Qf;
+  MatrixXd P_;
+
+  for(int i = 0; i < N; i++) {
+    P_ = Q + A.transpose() * P * A - A.transpose() * P * B * (R + B.transpose() * P * B).inverse() * B.transpose() * P * A;
+
+    if(P_ - P).maxCoeff() < EPS && (P - P_).maxCoeff() < EPS) break;
+
+    P = P_;
+  }
+  return P_;
+}
+
+/*
+LQR控制器
+@param robot_state
+@param refer_path
+@param s0
+@param A
+@param B
+@param Q
+@param R
+@return
+*/
+
+double LQRControl::lqrControl(vector<double> robot_state, vector<vector<double>> refer_path, double s0, MatrixXd A, MatrixXd B, MatrixXd Q, MatrixXd R) {
+  MatrixXd X(3, 1);
+  
+}
